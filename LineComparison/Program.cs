39be151f13975be0099f60f37b527ea73bd89b7d
Hyperival <sanjaygautam779@gmail.com>
2022-11-21 23:41:33 +0530

    using java equal methods to check equality

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -33,13 +33,17 @@ namespace LineComparison
             double Y2 = Convert.ToDouble(Console.ReadLine());
             double Linelength_02 = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
             Console.WriteLine("Linelength calculated is :{0}", Linelength_02);
-            if (Linelength_01.Equals(Linelength_02))
+            if (Linelength_01.CompareTo(Linelength_02) == 0)
             {
                 Console.WriteLine("Lines are equal");
             }
+            else if (Linelength_01.CompareTo(Linelength_02) > 0)
+            {
+                Console.WriteLine("Line 01 is greater than line 02");
+            }
             else
             {
-                Console.WriteLine("Lines are unequal");
+                Console.WriteLine("Line 02 is greater than line 01");
             }
         }
     }
