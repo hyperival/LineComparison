c43f32f31f63d4cd451543d73baed8f267a84eea
Hyperival <sanjaygautam779@gmail.com>
2022-11-21 23:39:14 +0530

    using java equal methods to check equality

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -11,6 +11,7 @@ namespace LineComparison
         public static void Main()
         {
             Console.WriteLine("Welcome to line comparision problem");
+            Console.WriteLine("Enter line 01 co-ordinates");
             Console.WriteLine("Enter start point of line  (x1) value in x axis");
             double x1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter start points of line  (y1) value in y axis");
@@ -19,8 +20,27 @@ namespace LineComparison
             double x2 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter end point of line (y2) value in y axis");
             double y2 = Convert.ToDouble(Console.ReadLine());
-            double lineLength = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("Linelength calculated is :" + lineLength);
+            double Linelength_01 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Linelength calculated is :{0}", Linelength_01);
+            Console.WriteLine("Enter line 02 co-ordinates");
+            Console.WriteLine("Enter start point of line  (X1) value in x axis");
+            double X1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter start points of line  (Y1) value in y axis");
+            double Y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter end point of line (X2) value in x axis");
+            double X2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter end point of line (Y2) value in y axis");
+            double Y2 = Convert.ToDouble(Console.ReadLine());
+            double Linelength_02 = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+            Console.WriteLine("Linelength calculated is :{0}", Linelength_02);
+            if (Linelength_01.Equals(Linelength_02))
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are unequal");
+            }
         }
     }
 }
